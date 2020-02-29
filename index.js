53263bb12db253b4c9b478be83f881ef813fdc2c
Eli Schiff <eli@elischiff.org>
2020-02-29 01:05:12 -0500

    fixed for github pages

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('website'))
+app.use(express.static('.'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
